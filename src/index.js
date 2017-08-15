783307b0b1a00ef554822121d53e449365de11cd
Armando Andini <armando.andini@hotmail.com>
2017-08-15 13:45:58 -0300

    Create a redux store, a reducer function and wrap the app into a Provider component.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, HashRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 import Voting from './Voting';
 import Results from './Results';
-import './index.css';
+
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Trainspotting', 'Ice Age'],
+      tally: {'Trainspotting': 2}
+    }
+  }
+})
 
 ReactDOM.render(
-  <HashRouter>
-    <div>
-      <Route exact path='/' component={Voting}/>
-      <Route path='/results' component={Results}/>
-    </div>
-  </HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <div>
+        <Route exact path='/' component={Voting}/>
+        <Route path='/results' component={Results}/>
+      </div>
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
